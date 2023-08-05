82afc7e993e09f03ea3702c75fa047b22ba6a44a
AndriyKy <akrechovetskiy@gmail.com>
2023-08-05 12:28:37 +0300

    feat: add __init__ to configs and utils

---
+++ utils/__init__.py
@@ -0,0 +1,5 @@
+from coords_converter import CoordinatesConverter
+from image_processing import (create_mask, extract_video_frame,
+                              retrieve_the_latest_frame)
+from logging_config import logging
+from tools import parse_time
