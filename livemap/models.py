92a4c9eba2ebcf8889c3ad7a41195ae714599a6c
andriyky <akrechovetskiy@gmail.com>
2023-10-11 12:37:27 +0300

    refactor: update models
    - Added `verbose_name_plural` in class Meta.
    - Added new fields to ParkingLot model.
    - Added getters to ParkingLot model.

--- livemap/models.py
+++ livemap/models.py
@@ -7,6 +7,9 @@ from django.db import models
 class Country(models.Model):
     country_name = models.CharField(max_length=100, unique=True)
 
+    class Meta:
+        verbose_name_plural = "Countries"
+
     def __str__(self) -> str:
         return str(self.country_name)
 
@@ -15,6 +18,9 @@ class City(models.Model):
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
     city_name = models.CharField(max_length=50)
 
+    class Meta:
+        verbose_name_plural = "Cities"
+
     def __str__(self) -> str:
         return f"{self.city_name}, {self.country}"
 
@@ -22,6 +28,9 @@ class City(models.Model):
 class Address(models.Model):
     city = models.ForeignKey(City, on_delete=models.CASCADE)
     parking_lot_address = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name_plural = "Addresses"
 
     def __str__(self) -> str:
         return f"{self.parking_lot_address}, {self.city}"
@@ -39,19 +48,37 @@ def validate_geolocation(geolocation: Any) -> None:
 
 
 class ParkingLot(models.Model):
+    class Answer(models.IntegerChoices):
+        NO = 0, "No"
+        YES = 1, "Yes"
+
     address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    total_spots = models.PositiveIntegerField()
+    spots_for_disabled = models.PositiveIntegerField(null=True, blank=True)
+    is_private = models.IntegerField(choices=Answer.choices, default=Answer.NO)
+    is_free = models.IntegerField(choices=Answer.choices, default=Answer.YES)
     geolocation = models.JSONField(
         validators=[validate_geolocation],
         help_text="Latitude and longitude of a parking lot. Ex.: [49.911848, 16.611212]",
     )
-    total_spots = models.PositiveIntegerField()
 
     def __str__(self) -> str:
         return str(self.address)
 
+    @property
+    def get_is_private(self) -> str:
+        # Get label from choices enum.
+        is_private_index = self.Answer.values.index(self.is_private)
+        return self.Answer.labels[is_private_index]
+
+    @property
+    def get_is_free(self) -> str:
+        is_free_index = self.Answer.values.index(self.is_free)
+        return self.Answer.labels[is_free_index]
+
 
 class VideoStreamSource(models.Model):
-    parking_lot = models.ForeignKey(ParkingLot, on_delete=models.CASCADE)
+    parking_lot = models.ForeignKey(ParkingLot, on_delete=models.CASCADE, related_name="stream_sources")
     stream_source = models.URLField()
     processing_rate = models.PositiveIntegerField(help_text="In seconds.")
     parking_zone = models.JSONField(
@@ -77,9 +104,12 @@ class VideoStreamSource(models.Model):
 
 
 class Occupancy(models.Model):
-    parking_lot = models.ForeignKey(ParkingLot, on_delete=models.CASCADE)
-    occupied_spots = models.PositiveIntegerField()
+    parking_lot = models.ForeignKey(ParkingLot, on_delete=models.CASCADE, related_name="occupancies")
+    occupied_spots = models.PositiveIntegerField(default=0)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Occupancy"
 
     def __str__(self) -> str:
         return f"{self.occupied_spots} occupied spots, {self.parking_lot}"
